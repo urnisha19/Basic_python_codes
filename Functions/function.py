a810c1265dad815efa50b021e254397edf1b5794
Nahia Nowreen Urnisha <urnisha09@gmail.com>
2022-08-10 00:52:28 +0600

    Function

---
+++ Functions/function.py
@@ -0,0 +1,26 @@
+# function define
+def f():
+    print("hello")
+
+
+f()  # function call
+
+
+# function define
+def sum(a, b):
+    return a + b
+
+
+o = sum(1, 2)  # function call
+print(o)
+
+
+# function define
+def print_my_name(name):
+    print("Your name is " + name)
+
+
+print("please enter your name")
+my_name = input()
+
+print_my_name(my_name)  # function call
---
+++ Functions/keyword_argument.py
@@ -0,0 +1,9 @@
+print("Hello")
+print("World")
+
+print("Hello ", end="")  # end="" skips new line
+print("world")
+
+print("a", "b", "c")
+
+print("a", "b", "c", sep=',') # sep skips space after every string
